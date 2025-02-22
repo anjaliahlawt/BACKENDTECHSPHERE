915f086a31e2e3e0e999e1e9028a9a88739be989
anjali ahlawat <anjaliahlawat45@gmail.com>
2025-02-22 22:52:10 +0530

    added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://techsphere-sigma.vercel.app/", 
   credentials: true, 
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
