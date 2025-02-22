cbf569b3c8172468fbca7a8d2fdaab0bf7925c0b
anjali ahlawat <anjaliahlawat45@gmail.com>
2025-02-22 21:56:55 +0530

    added

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(cors({
 const salt = await bcrypt.genSalt(10);
 console.log("Salt:", salt);
 
+const PORT = process.env.PORT || 3000;
+
 app.use("/otp", otpRouter);
 app.use("/auth", authRouter);
 app.use('/eventcard',eventcardRouter);
@@ -29,6 +31,6 @@ app.get("/", (req, res) => {
   res.send("Backend is running successfully! 🚀");
 });
 
-app.listen(3002, () => {
-  console.log("Server is running on port 3002");
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
