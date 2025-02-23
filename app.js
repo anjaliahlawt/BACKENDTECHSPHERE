0fcebfce799657a52370b06e81329be663ce0e66
anjali ahlawat <anjaliahlawat45@gmail.com>
2025-02-23 13:54:48 +0530

    allow all origin

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "*",  // Allow requests from any origin (all users)
+  origin: "https://techsphere-sigma.vercel.app/", 
   credentials: true, 
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 const salt = await bcrypt.genSalt(10);
 console.log("Salt:", salt);
